821bb487552ff7c22a75f98739e28a448aa688af
felip <felip@DESKTOP-0R5TRUD>
2023-04-06 00:18:22 -0300

    Add project files.

---
+++ TrianguloOrientadoObjetos/Program.cs
@@ -0,0 +1,17 @@
+﻿using TrianguloOrientadoObjetos;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        TrianguloObjetos trinagulo = new TrianguloObjetos();
+        trinagulo.lado1 = 8;
+        trinagulo.lado2= 5;
+        trinagulo.lado3 = 6;
+
+        Console.WriteLine(trinagulo.Triangulo());
+
+
+
+    }
+}
---
+++ TrianguloOrientadoObjetos/TrianguloObjetos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOrientadoObjetos
+{
+    internal class TrianguloObjetos
+    {
+       public double lado1;
+       public double lado2;
+       public double lado3;     
+        public string Triangulo()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                if (lado1 == lado2 && lado2 == lado3)
+                {
+                    return "Triangulo equilatero";
+                }
+                else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+                {
+                    return "Triangulo isosceles";
+                }
+                else
+                {
+                    return "Triangulo escaleno";
+                }
+            }
+            else
+            {
+                return "Não é possível formar um triangulo com isso.";
+            }
+        }
+    }
+
+}
+
+     
